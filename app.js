d3a5a978a64ea7ad612429056941d0a8c3dffc12
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-11 15:47:27 -0700

    Finished main functionalities

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 'use strict';
 
 //In-memory database of questions
-const questionList = [
+let questionList = [
 
   //First question
   {
     question: 'Inside which HTML element do we put the JavaScript?',
     answerOptions: [
-      '<scripting>',
-      ' <js>',
-      '<javascript>',
-      '<script>'
+      'scripting',
+      ' js',
+      'javascript',
+      'script'
     ],
-    correctAnswer: '<script>',
+    correctAnswer: 'script',
     answerSelect: 1,
     questionAsked: false,
   },
@@ -97,12 +97,119 @@ const questionList = [
 ];
 
 //Initial store
-const store = {
+let store = {
   view: 'start',
   score: 0,
+  currentQuestionCount: 1,
   submitBtnClicked: false,
   quizCompleted: false,
 };
+
+function defaultStore() {
+  return {
+    view: 'start',
+    score: 0,
+    currentQuestionCount: 1,
+    submitBtnClicked: false,
+    quizCompleted: false,
+  };
+}
+
+function defaultQuestionList() {
+  return [ //First question
+    {
+      question: 'Inside which HTML element do we put the JavaScript?',
+      answerOptions: [
+        'scripting',
+        ' js',
+        'javascript',
+        'script'
+      ],
+      correctAnswer: 'script',
+      answerSelect: 1,
+      questionAsked: false,
+    },
+    //Second question
+    {
+      question: 'What is an argument?',
+      answerOptions: [
+        'The function’s parameters upon declaration',
+        'A variable that gets passed to the function when its called.',
+        'A function that has no parameters',
+        'A variable assigned to a function'
+      ],
+      correctAnswer: 'A variable that gets passed to the function when its called.',
+      answerSelect: 2,
+      questionAsked: false,
+    },
+    //Third question
+    {
+      question: 'Which value is ‘falsy’?',
+      answerOptions: [
+        '2',
+        '“False”',
+        '“”',
+        '‘Truthy’'
+      ],
+      correctAnswer: '""',
+      answerSelect: 3,
+      questionAsked: false,
+    },
+    //Fourth Question
+    {
+      question: 'what is typeof x for: var y = 1, x = y = typeof x;',
+      answerOptions: [
+        '"number"',
+        'undefined',
+        '"undefined"',
+        'x'
+      ],
+      correctAnswer: '"undefined"',
+      answerSelect: 4,
+      questionAsked: false,
+    },
+    //Fifth Question
+    {
+      question: 'Which will correctly comment out the text?',
+      answerOptions: [
+        '// “Test Text” //',
+        '/“Test Text”/',
+        '*/ “Test Text” /*',
+        '\\ “Test Text \\'
+      ],
+      correctAnswer: '// “Test Text” //',
+      answerSelect: 5,
+      questionAsked: false,
+    },
+    //Sixth Question
+    {
+      question: 'For ECMAscript, what does ECMA stand for?',
+      answerOptions: [
+        'European Association for Standardizing Information and Communication Systems',
+        'European Council for Making and Arranging JavaScript Principles',
+        'European Association for Strategizing Implementation of Communication Systems',
+        'European Antarctican Massachusetts Alliance for JavaScript'
+      ],
+      correctAnswer: 'European Association for Standardizing Information and Communication Systems',
+      answerSelect: 6,
+      questionAsked: false,
+    },
+    //Seventh Question
+    {
+      question: 'What does the following run in the console: "The morning is upon us.".slice(-3)',
+      answerOptions: [
+        '"us."',
+        'undefined',
+        '"the morning is upon us"',
+        'us'
+      ],
+      correctAnswer: '"us."',
+      answerSelect: 7,
+      questionAsked: false,
+    },
+  ];
+}
+  
 
 
 //Template generators
@@ -132,28 +239,28 @@ function generateRandomQuestion() {
   // counter = arr.length
 
   //
-//   questionList[random].questionAsked = true;
-//   //
-// // retrieve how many of ur obj have questionAsked set to true.
-// //
-//
-//   let s = questionList.forEach(function (obj) {
-//     console.log("nope?");
-//     console.log(obj.questionAsked);
-//      return obj.questionAsked === true;
-//   });
-//
-//   console.log("!!!!");
-//   console.log(s);
-//
-//   // if (counter !== questionList.length){
-//   //   // return
-//   // }
-//   //
-//   // if (randomQuestion.questionAsked === true) {
-//   //   counter++;
-//   //   generateRandomQuestion();
-//   // }
+  //   questionList[random].questionAsked = true;
+  //   //
+  // // retrieve how many of ur obj have questionAsked set to true.
+  // //
+  //
+  //   let s = questionList.forEach(function (obj) {
+  //     console.log("nope?");
+  //     console.log(obj.questionAsked);
+  //      return obj.questionAsked === true;
+  //   });
+  //
+  //   console.log("!!!!");
+  //   console.log(s);
+  //
+  //   // if (counter !== questionList.length){
+  //   //   // return
+  //   // }
+  //   //
+  //   // if (randomQuestion.questionAsked === true) {
+  //   //   counter++;
+  //   //   generateRandomQuestion();
+  //   // }
 
   console.log(random);
   //Match randomQuestion's answerSelect value to the answerSelect
@@ -187,7 +294,12 @@ function render(){
 function generateAnswerList(QuestionGenerator) {
   QuestionGenerator();
   console.log(randomQuestion);
-  $('.question-title-container').html(`${randomQuestion.question}`)
+  $('.question-title-container').html(
+    `<div class='question-counter'>
+    Question # ${store.currentQuestionCount}
+    </div>
+    ${randomQuestion.question}`);
+
   $('.question-list').html(`
   <div class='input-section'>
   <input type="radio" name="clicked-question" class='radio-btn' value='${randomQuestion.answerOptions[0]}'>
@@ -218,6 +330,7 @@ function handleStartButton() {
   $('.start').on('click','.js-start-btn', function () {
     console.log('js-start-btn was clicked.');
     store.view = 'quiz';
+    updateCounter();
     render();
   });
 }
@@ -225,23 +338,61 @@ function handleStartButton() {
 function handleSubmitButton () {
   $('#quiz-form').submit(function (event) {
     event.preventDefault();
+    let submittedAnswer = $('input[name=\'clicked-question\']:checked').val();   
+    console.log(submittedAnswer); 
     console.log('js-submit-btn was clicked.');
     store.submitBtnClicked = true;
     console.log(store.submitBtnClicked);
+    if (submittedAnswer === randomQuestion.correctAnswer) {
+      console.log('You got it right!');
+      userCorrectAnswerSubmitted(submittedAnswer);
+    }
+    else {
+      userInCorrectAnswerSubmitted(submittedAnswer);
+    }
+    updateCounter();
   });
+}
+
+//Create a function that handles if the user was incorrect
+
+function userInCorrectAnswerSubmitted(answer) {
+  $('.correct-updater').html(`Incorrect! The correct answer is: ${randomQuestion.correctAnswer}`);
+}
+
+//Create a function that handles if the user was correct
+function userCorrectAnswerSubmitted(answer) {
+  store.score++;
+  $('.correct-updater').html(`Correct!`);
+
+}
+
+//Function that updates counter after each question
+function updateCounter() {
+  if (store.view === 'results') {
+    $('.correct-counter').html(`Quiz Results: ${store.score} / ${questionList.length}`);
+  }
+  else {
+    $('.correct-counter').html(`Progress: ${store.score} / ${questionList.length}`);
+  }
 }
 
 function handleNextQuestionButton () {
   $('.quiz').on('click','.js-next-question-btn', function () {
     console.log('js-next-question-btn was clicked.');
     event.preventDefault();
+    $('.correct-updater').html('');
     if (store.submitBtnClicked === true) {
+      ++store.currentQuestionCount;
       store.submitBtnClicked = false;
       console.log(store.submitBtnClicked);
       generateAnswerList(generateRandomQuestion);
     }
     if (store.quizCompleted === true) {
       store.view = 'results';
+      store.currentQuestionCount = 1;
+      // generateAnswerList(QuestionGenerator);
+      updateCounter();
       render();
     }
   });
@@ -251,7 +402,19 @@ function handleStartOverButton () {
   $('.results').on('click','.js-startover-btn', function () {
     console.log('js-startover-btn was clicked.');
     store.view = 'start';
+    // store.score = 0;
+    // store.currentQuestionCount = 1;
+    store = defaultStore();
+    questionList = defaultQuestionList();
+    // updateCounter();
+    console.log(store.currentQuestionCount);
+    $('.question-title-container').html(
+      `<div class='question-counter'>
+      Question # ${store.currentQuestionCount}
+      </div>
+      ${randomQuestion.question}`);
     render();
+    console.log(store.currentQuestionCount);
   });
 }
 //Function that initializes all event listeners when DOM is ready
