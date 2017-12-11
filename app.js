36a5a27139b705a9730323f02485cb89905ea759
Vernon Mensah <vernonmensah@gmail.com>
2017-12-11 12:37:22 -0500

    Added event listeners for buttons.

--- app.js
+++ app.js
@@ -37,12 +37,62 @@ function render(){
 
 
 //Event handlers
+function nextScene () {
+
+}
+
+function handleStartButton() {
+
+  $(".start").on("click",".js-start-btn", function () {
+    console.log("js-start-btn was clicked.");
+
+    store.view = "quiz";
+
+    render();
+
+  });
 
 
+}
 
+function handleSubmitButton () {
+  $("#quiz-form").submit(function (event) {
+    event.preventDefault();
+    console.log("js-submit-btn was clicked.");
+
+    render();
+
+  });
+}
+
+function handleNextQuestionButton () {
+  $(".quiz").on("click",".js-next-question-btn", function () {
+    console.log("js-next-question-btn was clicked.");
+
+    store.view = "results";
+    render();
+
+  });
+}
+
+function handleStartOverButton () {
+  $(".results").on("click",".js-startover-btn", function () {
+    console.log("js-startover-btn was clicked.");
+
+    store.view = "start";
+
+    render();
+
+  });
+}
 //Function that initializes all event listeners when DOM is ready
 function handleQuizEventListeners() {
-  render();  
+  nextScene();
+  handleStartButton();
+  handleNextQuestionButton();
+  handleSubmitButton();
+  handleStartOverButton();
+  render();
 }
 
 $(handleQuizEventListeners);
