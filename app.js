8b7b71e17010e82a01d424309f40f17c005517c7
Vernon Mensah <vernonmensah@gmail.com>
2017-12-11 12:50:34 -0500

    Added questions temp

--- app.js
+++ app.js
@@ -3,8 +3,55 @@
 //In-memory database of questions
 const questionList = [
 
+  //First question
+  {
+    question: 'What planet is Rey from?',
+    answerOptions: [
+      'Jakku',
+      'Tatooine',
+      'Endor',
+      'Taris'
+    ],
+    correctAnswer: 'Jakku',
+    answerSelect: 1
+  },
+  //Second question
+  {
+    question: 'What color is Lukes lightsaber in Return of the jedi?',
+    answerOptions: [
+      'Purple',
+      'Blue',
+      'Green',
+      'Red'
+    ],
+    correctAnswer: 'Green',
+    answerSelect: 2
+  },
+  //Third question
+  {
+    question: 'What is the group of dark side users that Kylo Ren leads?',
+    answerOptions: [
+      'Knights of Ren',
+      'The First Order',
+      'The Sith Order',
+      'Acolytes of Snoke'
+    ],
+    correctAnswer: 'Knights of Ren',
+    answerSelect: 3
+  },
+  //Fourth Question
+  {
+    question: 'What movie does Emperor Palpatine first appear in?',
+    answerOptions: [
+      'Episode 6',
+      'Episode 5',
+      'Episode 1',
+      'Episode 4'
+    ],
+    correctAnswer: 'Episode 5',
+    answerSelect: 4
+  },
 ];
-
 //Initial store
 const store = {
   view: 'start',
