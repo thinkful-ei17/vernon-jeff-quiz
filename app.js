bb26feec227ce6543cfa688b06eff920047df597
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-11 10:10:12 -0700

    Added initial rendering function and JS comments

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
 'use strict';
 
-console.log('hello');
+//In-memory database of questions
+const questionList = [
+
+];
+
+//Initial store
+const store = {
+  view: 'start',
+  score: 0,
+};
+
+
+//Template generators
+
+
+//Rendering Functions
+function render(){
+  if (store.view === 'start'){
+    $('.start').show();
+    $('.quiz').hide();
+    $('.results').hide();
+  }
+  else if (store.view === 'quiz') {
+    $('.quiz').show();
+    $('.start').hide();
+    $('.results').hide();
+  }
+  else if (store.view === 'results') {
+    $('.results').show();
+    $('.start').hide();
+    $('.quiz').hide();
+  }
+}
+
+
+
+//Event handlers
+
+
+
+//Function that initializes all event listeners when DOM is ready
+function handleQuizEventListeners() {
+  render();  
+}
+
+$(handleQuizEventListeners);
